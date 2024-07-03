06a7796de9a32362b25dbfe02cea7536a3e57ed8
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-03 15:40:57 -0300

    Páginas e componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <h2>Movies lib</h2>
+      <Outlet />
     </>
   )
 }
